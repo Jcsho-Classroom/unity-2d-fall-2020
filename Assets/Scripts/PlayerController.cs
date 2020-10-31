d32e726d5ab1720d430c18b40fbd18205526818f
Justin Ho <justinhodev@gmail.com>
2020-10-31 01:30:45 -0700

    feat: add player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rb2d;
+    private float _horizontal;
+    private float _vertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get input
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Move(_horizontal, _vertical);
+    }
+
+    public void Move(float x, float y)
+    {
+        // move the player
+        Vector2 velocity = new Vector2(x, y);
+        _rb2d.MovePosition(_rb2d.position + velocity * Time.fixedDeltaTime);
+    }
+}
