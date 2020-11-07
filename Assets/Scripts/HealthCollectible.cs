e84077f9a76020607faeefbdc8d44fa530af4f25
DemoJustin <72777477+DemoJustin@users.noreply.github.com>
2020-11-07 12:05:10 -0800

    Week 4
    Health and Damage

---
+++ Assets/Scripts/HealthCollectible.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    public int healthAmount = 1;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController>();
+
+        if (controller != null)
+        {
+            if (controller.currentHealth < controller.maxHealth)
+            {
+                controller.ChangeHealth(healthAmount);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 2;
-    
+    [Range(1, 100)]
+    public int maxHealth = 5;
+    public int currentHealth;
+
+    private Animator _animator;
     private Rigidbody2D _rb2d;
     private float _horizontal;
     private float _vertical;
@@ -13,7 +18,9 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _animator = GetComponent<Animator>();
         _rb2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -22,6 +29,10 @@ public class PlayerController : MonoBehaviour
         // get input
         _horizontal = Input.GetAxis("Horizontal");
         _vertical = Input.GetAxis("Vertical");
+
+        _animator.SetFloat("Look X", _horizontal);
+        _animator.SetFloat("Look Y", _vertical);
+        
     }
 
     void FixedUpdate()
@@ -33,6 +44,12 @@ public class PlayerController : MonoBehaviour
     {
         // move the player
         Vector2 velocity = new Vector2(x, y);
+        _animator.SetFloat("Speed", Math.Abs(velocity.x + velocity.y));
         _rb2d.MovePosition(_rb2d.position + velocity * (speed * Time.fixedDeltaTime));
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
     }
 }
